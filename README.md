dec80a38267e6a4a139c152382893951d3d972dc
Kumail Baloch <kumaillatif60@gmail.com>
2023-05-13 17:40:55 +0500

    Updated SelectMultipleWeekDays Component

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Expo: expo install react-native-select-week-day
 ![Week Selector demo](https://user-images.githubusercontent.com/56933027/213881616-d645f679-1e1c-4ed6-8293-d70b45b339e8.gif)
 
 ## Sample code
-```
+```javascript
 import {SelectWeekDay} from 'react-native-select-week-day'
 export default function App(){
     return(
@@ -17,7 +17,7 @@ export default function App(){
             containerStyle={{justifyContent: 'center', alignItems: 'center',marginVertical: 60}}
             bubblesInitialColor={'green'}
             bubblesSelectedColor={'red'}
-            bubbleStyle={{width: 50,height: 50}}
+            size={50}
             onChange={(day)=>alert(day)}
         />
     );
--- src/selectMultipleWeekDays.js
+++ src/selectMultipleWeekDays.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
@@ -6,7 +6,7 @@ import {
     TouchableOpacity as Button
 } from 'react-native';
 
-SelectWeekDay.defaultProps = {
+SelectMultipleWeekDays.defaultProps = {
     bubblesInitialColor: 'blue',
     bubblesSelectedColor: 'red',
     size: 20,
@@ -22,66 +22,56 @@ export default function SelectMultipleWeekDays({
     noOfBubblesPerLine,
     size,
     onChange,
-}){
-    const [week, setWeek] = useState(['Sun','Mon','Tue','Wed','Thu','Fri','Sat']);
+}) {
+    const [week, setWeek] = useState(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
     const [dayChoose, setDayChoose] = useState([])
-    
+    var days = []
+
     const getColor = (index) => {
-        if(dayChoose.length!=0){
-            if(dayChoose.includes(index)) return bubblesSelectedColor
+        if (dayChoose.length != 0) {
+            if (dayChoose.includes(index)) return bubblesSelectedColor
             else return bubblesInitialColor
-        }else{
+        } else {
             return bubblesInitialColor
         }
     }
-    function selectDays(endIndex) {
-        let i = dayChoose[0]+1;
-        while (i !== endIndex) {
-          if (i === week.length) {
-            i = 0;
-          }
-          setDayChoose([...dayChoose,i])
-          i++;
+    function toggleElement(arr, elem) {
+        if (arr.includes(elem)) {
+            return arr.filter((item) => (item != elem))
+        } else {
+            return [...arr, elem];
         }
-        setDayChoose([...dayChoose,endIndex])
-      }
-      
-      
-   return (
-       <View style={[{},containerStyle]}>
-          <FlatList
-            numColumns={noOfBubblesPerLine?noOfBubblesPerLine:4}
-            data={week}
-            renderItem={({item})=>{
-                return(
-                    <Button
-                        activeOpacity={0.6} 
-                        onPress={()=>{
-                            if(dayChoose.length>2) setDayChoose([week.indexOf(item),])
-                            else if(dayChoose.length <= 1){
-                                setDayChoose([...dayChoose,week.indexOf(item)])
-                            }
-                            else{
-                                selectDays(week.indexOf(item))
-                            }
-                            onChange?onChange(dayChoose):null
-                        }}
-                        style={[{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            borderRadius:75,
-                            padding: 10,
-                            margin: 5,
-                            height: size?size:null,
-                            width: size?size:null,
-                            backgroundColor: getColor(week.indexOf(item))
-                        },bubbleStyle]}>
-                        <Text style={[{color: 'white'},bubbleTextStyle]}>{item}</Text>
-                    </Button>
-                );
-            }}
-            keyExtractor={(item)=>week.indexOf(item)}
-          />
-       </View>
-   );
+    }
+    return (
+        <View style={[{}, containerStyle]}>
+            <FlatList
+                numColumns={noOfBubblesPerLine ? noOfBubblesPerLine : 4}
+                data={week}
+                renderItem={({ item }) => {
+                    return (
+                        <Button
+                            activeOpacity={0.6}
+                            onPress={() => {
+                                setDayChoose(toggleElement(dayChoose, week.indexOf(item)))
+                                days = toggleElement(dayChoose, week.indexOf(item))
+                                onChange ? onChange(days) : null
+                            }}
+                            style={[{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderRadius: 75,
+                                padding: 10,
+                                margin: 5,
+                                height: size ? size : null,
+                                width: size ? size : null,
+                                backgroundColor: getColor(week.indexOf(item))
+                            }, bubbleStyle]}>
+                            <Text style={[{ color: 'white' }, bubbleTextStyle]}>{item}</Text>
+                        </Button>
+                    );
+                }}
+                keyExtractor={(item) => week.indexOf(item)}
+            />
+        </View>
+    );
 }
